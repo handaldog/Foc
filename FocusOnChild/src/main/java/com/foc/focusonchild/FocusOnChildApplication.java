48c24870a8ba1f662ea6fdaf44b266d1a3807ad5
handaldog <handal@kakao.com>
2023-07-12 21:11:51 +0900

    original status

---
+++ FocusOnChild/src/main/java/com/foc/focusonchild/FocusOnChildApplication.java
@@ -0,0 +1,13 @@
+package com.foc.focusonchild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FocusOnChildApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FocusOnChildApplication.class, args);
+    }
+
+}
---
+++ FocusOnChild/src/test/java/com/foc/focusonchild/FocusOnChildApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foc.focusonchild;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FocusOnChildApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
